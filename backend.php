b47a6315f11ff5f2ce51d01d4ae3a97b597ef4d7
Takuto <htakutolife@gmail.com>
2023-04-24 21:32:33 +0900

    ログイン機能

--- backend.php
+++ backend.php
@@ -1,9 +1,15 @@
+<?php
+session_start();
+if (!isset($_SESSION['id'])){
+  header('Location: login.php');
+}
+?>
 <!doctype html>
 <html lang="ja">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Blog Backend</title>
+    <title>TakutoBlog</title>
 
     <!-- Bootstrap core CSS -->
     <link href="./css/bootstrap.min.css" rel="stylesheet">
@@ -38,7 +44,7 @@
 
 <nav class="navbar navbar-expand-md navbar-dark bg-red fixed-top">
   <div class="container">
-    <a class="navbar-brand" href="/blog/backend.php">My Blog Backend</a>
+    <a class="navbar-brand" href="/blog/backend.php">Takuto Blog Backend</a>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item"><a class="nav-link" href="#">記事を書く</a></li>
